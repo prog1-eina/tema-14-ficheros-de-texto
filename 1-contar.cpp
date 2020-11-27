02d1017b71db990180ad93124a70037bd743ba79
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-11-27 20:07:25 +0100

    Unsigned int y string

--- 1-contar.cpp
+++ 1-contar.cpp
@@ -1,43 +1,44 @@
-/********************************************************************************\
+/******************************************************************************\
  * Curso de Programación 1. Tema 14 (Ficheros de texto)
  * Autores: Javier Martínez y Miguel Ángel Latre
  * Última revisión: 26 de noviembre de 2018
  * Resumen: Algunas funciones que trabajan con ficheros de texto.
- * Codificación de caracteres original de este fichero: UTF-8 con BOM
-\********************************************************************************/
+\******************************************************************************/
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 /*
- * Pre:  «nombreFichero» es el nombre de un fichero de texto existente y accesible
- *       para su lectura.
+ * Pre:  «nombreFichero» es el nombre de un fichero de texto existente y
+ *       accesible para su lectura.
  * Post: Ha asignado a «nLineas» el número de líneas del fichero y a
  *       «nCaracteres», el número de caracteres del mismo.
  * Nota: Solución que lee línea a línea.
  */
-void contabilizar(const char nombreFichero[], int& nLineas,
+void contabilizar(const string nombreFichero, int& nLineas,
                   int& nCaracteres) {
-    const int MAX = 128;                  // Máxima longitud estimada de una línea
-    char linea[MAX];                      // Para almacenar las líneas leídas
-    ifstream f;                           // Declara un flujo de entrada.
-    nLineas = 0;
-    nCaracteres = 0;                      // Solución inicial provisional
-    f.open (nombreFichero);  // Asocia a «f» el fichero de nombre «nombreFichero».
+    ifstream f;                      // Declara un flujo de entrada.
+    f.open (nombreFichero);          // Asocia a «f» el fichero «nombreFichero».
     if (f.is_open()) {
-        f.getline(linea, MAX);            // Intenta leer una primera línea.
+        nLineas = 0;
+        nCaracteres = 0;                  // Solución inicial provisional
+        string linea;                     // Para almacenar las líneas leídas
+        getline(f, linea);                // Intenta leer una primera línea.
         while (!f.eof()) {
             // Mientras el último intento de lectura fue correcto
-            nLineas = nLineas + 1;        // Actualiza el número de líneas...
-            nCaracteres = nCaracteres + f.gcount();           // ... y caracteres.
-            f.getline(linea, MAX);        // Intenta leer una nueva línea.
+            nLineas++;                    // Actualiza el número de líneas...
+            nCaracteres += linea.length() + 1;           // ... y caracteres.
+            getline(f, linea);            // Intenta leer una nueva línea.
         }
         f.close();                        // Libera el fichero asociado a «f».
     }
     else {
         cerr << "No se ha podido abrir el fichero \"" << nombreFichero << "\"."
              << endl;
+        nLineas = -1;
+        nCaracteres = -1;
     }
 }
 
@@ -48,13 +49,13 @@ void contabilizar(const char nombreFichero[], int& nLineas,
  *       a «nCaracteres» el número de caracteres del mismo.
  * Nota: Solución que lee carácter a carácter.
  */
-void contabilizar2(const char nombreFichero[], int& nLineas,
-                  int& nCaracteres) {
-    nLineas = 0;
-    nCaracteres = 0;                    // Solución inicial provisional
+void contabilizar2(const string nombreFichero,
+                   int& nLineas, int& nCaracteres) {
     ifstream f;                         // Declara un flujo de entrada.
     f.open(nombreFichero);   // Asocia a «f» el fichero de nombre «nombreFichero».
     if (f.is_open()) {
+        nLineas = 0;
+        nCaracteres = 0;                // Solución inicial provisional
         char c;
         f.get(c);                       // Intenta leer un primer carácter de «f».
         while(!f.eof()) {
@@ -70,6 +71,8 @@ void contabilizar2(const char nombreFichero[], int& nLineas,
     else {
         cerr << "No se ha podido abrir el fichero \"" << nombreFichero << "\"."
              << endl;
+        nLineas = -1;
+        nCaracteres = -1;
     }
 }
 
@@ -81,7 +84,7 @@ void contabilizar2(const char nombreFichero[], int& nLineas,
  *       fichero «lope-de-vega.txt» y mostrando los resultados obtenidos.
  */
 int main() {
-    const char NOMBRE_FICHERO[] = "../lope-de-vega.txt";
+    const string NOMBRE_FICHERO = "lope-de-vega.txt";
 
     int lineas, caracteres;
     contabilizar(NOMBRE_FICHERO, lineas, caracteres);
